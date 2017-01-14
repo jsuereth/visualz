31e939a511f8a5c768921903df6f78c80f65f470
Josh Suereth <joshua.suereth@gmail.com>
2017-01-14 14:22:08 -0500

    Add 3d simplex noise.

--- src/main/scala/com/jsuereth/visualz/plasma/Plasma.scala
+++ src/main/scala/com/jsuereth/visualz/plasma/Plasma.scala
@@ -11,23 +11,25 @@ object Plasma {
   def main(args: Array[String]): Unit = {
     val width = 256
     val height = 256
-    val noiseGen = SimplexNoise.random()
-    val img = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR)
+    val time = 16
+    val noiseGen = SimplexNoise.default
     def color(noise: Double): Int = {
       val size = (noise * 100).toInt + 100
       (new java.awt.Color(size, size, size)).getRGB
     }
-    for {
-      x <- 0 until width
-      y <- 0 until height
-      px = (x.toFloat / width)
-      py = (y.toFloat / height)
-      noise = noiseGen(px,py)
-    } img.setRGB(x,y,color(noise))
-
-
-    val outputfile = new File("plasma.png")
-    ImageIO.write(img, "png", outputfile)
+    for (t <- 0 until time) {
+      val img = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR)
+      for {
+        x <- 0 until width
+        y <- 0 until height
+        px = (x.toFloat / width)
+        py = (y.toFloat / height)
+        pt = 0.5f + (t * 0.1f)
+        noise = noiseGen(px, py, pt)
+      } img.setRGB(x, y, color(noise))
+      val outputfile = new File(s"plasma-${t}.png")
+      ImageIO.write(img, "png", outputfile)
+    }
   }
 }
 
--- src/main/scala/com/jsuereth/visualz/plasma/SimplexNoise.scala
+++ src/main/scala/com/jsuereth/visualz/plasma/SimplexNoise.scala
@@ -46,11 +46,12 @@ object SimplexNoise {
   }
 
   private def dot(g: Gradient, x: Double, y: Double): Double = g.x * x + g.y * y
+  private def dot(g: Gradient, x: Double, y: Double, z: Double): Double = g.x * x + g.y * y + g.z * z
 
-  private val noise2d = new SimplexNoise(grad3)
+  val default = new SimplexNoise(grad3)
 
   /** Grabs simplex noise using built in gradients. */
-  def noise(x: Double, y: Double): Double = noise2d(x, y)
+  def noise(x: Double, y: Double): Double = default(x, y)
 
   /** Creates simplex noise using random grandients. */
   def random(rand: java.util.Random = new java.util.Random()): SimplexNoise = {
@@ -116,10 +117,10 @@ class SimplexNoise(gradients: Array[Gradient]) {
     val n1 =
       if (t1 < 0) 0.0
       else {
-        t1 *= t1;
+        t1 *= t1
         t1 * t1 * dot(gradients(gi1), x1, y1)
       }
-    var t2 = 0.5 - x2 * x2 - y2 * y2;
+    var t2 = 0.5 - x2 * x2 - y2 * y2
     val n2 =
       if (t2 < 0) 0.0
       else {
@@ -129,6 +130,85 @@ class SimplexNoise(gradients: Array[Gradient]) {
     // Add contributions from each corner to get the final noise value.
     // The result is scaled to return values in the interval [-1,1].
     70.0 * (n0 + n1 + n2)
+  }
+
+  // 3D simplex noise
+  def apply(xin: Double, yin: Double, zin: Double): Double = {
+    // Skew the input space to determine which simplex cell we're in
+    val s = (xin+yin+zin)*F3 // Very nice and simple skew factor for 3D
+    val i = fastfloor(xin+s)
+    val j = fastfloor(yin+s)
+    val k = fastfloor(zin+s)
+    val t = (i+j+k)*G3
+    val X0 = i-t // Unskew the cell origin back to (x,y,z) space
+    val Y0 = j-t
+    val Z0 = k-t
+    val x0 = xin-X0 // The x,y,z distances from the cell origin
+    val y0 = yin-Y0
+    val z0 = zin-Z0
+    // For the 3D case, the simplex shape is a slightly irregular tetrahedron.
+    // Determine which simplex we are in.
+    val (i1, j1, k1, i2, j2, k2) =
+      if(x0>=y0) {
+        if(y0>=z0)
+          (1, 0, 0, 1, 1, 0)  // X Y Z order
+        else if(x0>=z0) (1, 0, 0, 1, 0, 1) // X Z Y order
+        else (0, 0, 1, 1, 0, 1) // Z X Y order
+      }
+      else { // x0<y0
+        if(y0<z0) (0, 0, 1, 0, 1, 1) // Z Y X order
+        else if(x0<z0) (0, 1, 0, 0, 1, 1) // Y Z X order
+        else (0, 1, 0, 1, 1, 0)  // Y X Z order
+      }
+    // A step of (1,0,0) in (i,j,k) means a step of (1-c,-c,-c) in (x,y,z),
+    // a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z), and
+    // a step of (0,0,1) in (i,j,k) means a step of (-c,-c,1-c) in (x,y,z), where
+    // c = 1/6.
+    val x1 = x0 - i1 + G3 // Offsets for second corner in (x,y,z) coords
+    val y1 = y0 - j1 + G3
+    val z1 = z0 - k1 + G3
+    val x2 = x0 - i2 + 2.0*G3 // Offsets for third corner in (x,y,z) coords
+    val y2 = y0 - j2 + 2.0*G3
+    val z2 = z0 - k2 + 2.0*G3
+    val x3 = x0 - 1.0 + 3.0*G3 // Offsets for last corner in (x,y,z) coords
+    val y3 = y0 - 1.0 + 3.0*G3
+    val z3 = z0 - 1.0 + 3.0*G3
+    // Work out the hashed gradient indices of the four simplex corners
+    val ii = i & 255
+    val jj = j & 255
+    val kk = k & 255
+    val gi0 = permMod12(ii+perm(jj+perm(kk)))
+    val gi1 = permMod12(ii+i1+perm(jj+j1+perm(kk+k1)))
+    val gi2 = permMod12(ii+i2+perm(jj+j2+perm(kk+k2)))
+    val gi3 = permMod12(ii+1+perm(jj+1+perm(kk+1)))
+    // Calculate the contribution from the four corners
+    var t0 = 0.6 - x0*x0 - y0*y0 - z0*z0
+    val n0 = if(t0<0) 0.0
+    else {
+      t0 *= t0
+      t0 * t0 * dot(grad3(gi0), x0, y0, z0)
+    }
+    var t1 = 0.6 - x1*x1 - y1*y1 - z1*z1
+    val n1 = if(t1<0) 0.0
+    else {
+      t1 *= t1
+      t1 * t1 * dot(gradients(gi1), x1, y1, z1)
+    }
+    var t2 = 0.6 - x2*x2 - y2*y2 - z2*z2
+    val n2 = if(t2<0) 0.0
+    else {
+      t2 *= t2
+      t2 * t2 * dot(gradients(gi2), x2, y2, z2)
+    }
+    var t3 = 0.6 - x3*x3 - y3*y3 - z3*z3
+    val n3 = if(t3<0) 0.0
+    else {
+      t3 *= t3
+      t3 * t3 * dot(gradients(gi3), x3, y3, z3)
+    }
+    // Add contributions from each corner to get the final noise value.
+    // The result is scaled to stay just inside [-1,1]
+    32.0*(n0 + n1 + n2 + n3)
   }
 }
 
